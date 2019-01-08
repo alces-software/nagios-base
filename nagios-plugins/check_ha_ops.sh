3af1d0e397deeb39b0320fb2d38832cea46bb4c6
Ross Rodwell <ross.rodwell@alces-software.com>
2019-01-08 12:33:59 +0000

    Added nagios plugin to detect for "warning: Processing failed op" in /var/log/messages

---
+++ nagios-plugins/check_ha_ops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash                                                                                                                                                            
+                                                                                                                          
+
+################################################################################
+# (c) Copyright 2018 Stephen F Norledge & Alces Software Ltd.                  #
+#                                                                              #
+# HPC Cluster Toolkit                                                          #
+#                                                                              #
+# This file/package is part of the HPC Cluster Toolkit                         #
+#                                                                              #
+# This is free software: you can redistribute it and/or modify it under        #
+# the terms of the GNU Affero General Public License as published by the Free  #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# This file is distributed in the hope that it will be useful, but WITHOUT     #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License #
+# for more details.                                                            #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this product.  If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                              #
+# For more information on Alces Software, please visit:                        #
+# http://www.alces-software.org/                                               #
+#                                                                              #
+################################################################################
+
+
+grep "$(date +"%b %e").*warning: Processing failed op" /var/log/messages
+rc=$?
+
+if [ "${rc}" -eq "0" ]; then
+    echo "Processing failed op detected"
+    exit 2
+else
+    echo "No processing failed op detected"
+    exit 0
+fi
+
