66fdd6c1606ecb982e9498b7703bf9a3d4f59ad3
Ross Rodwell <ross.rodwell@alces-software.com>
2019-04-09 11:48:01 +0100

    Reworked parameter syntax.

--- nagios-plugins/check_ping.sh
+++ nagios-plugins/check_ping.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-./check_ping $1
+host=$2
+warning=$4
+critical=$6
+nr_packets=$8
+
+echo "host: $host"
+echo "warning: $warning"
+echo "critical: $critical"
+echo "nr_packets: $nr_packets"
+
+./check_ping -H $host -w $warning -c $critical -p $nr_packets
 
 rc=$?
 
