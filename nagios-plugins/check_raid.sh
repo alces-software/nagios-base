a98082a766aa62671a06239845ebcda5e6d562ed
Ross Rodwell <ross.rodwell@alces-software.com>
2019-10-29 16:59:43 +0000

    Added RAID check plugin wrapper.

---
+++ nagios-plugins/check_raid.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# This script attempts to determine the manner in which RAID health
+# should be checked, then calls the appropriate check.
+#
+
+hpssacli_present=0
+
+if [ -x /usr/sbin/hpssacli ]; then
+   # echo "hpssacli installed."
+   hpssacli_present=1
+else
+   # echo "No hpssacli detected."
+   hpssacli_present=0
+fi
+
+softwareraid=0
+
+if [ $(grep -ci "md[[:digit:]]" /proc/mdstat) -ge 1 ]; then
+   # echo "Software RAID configured."
+   softwareraid=1
+else
+   # echo "No software RAID configured."
+   softwareraid=0
+fi
+
+#
+# Executes if HW RAID (using HPSSACLI) in use and SW RAID is NOT.
+#
+if [ ${hpssacli_present} -eq 1 ] && [ ${softwareraid} -eq 0 ]; then
+   /opt/nagios/nagios-plugins/check_hpe_SA_RAID
+fi
+
+#
+# Executes if HW RAID (using HPSSACLI) is not in use and SW RAID IS.
+#
+if [ ${hpssacli_present} -eq 0 ] && [ ${softwareraid} -eq 1 ]; then
+   /opt/nagios/nagios-plugins/check_swraid1.sh
+fi 
+
+exit 3
