b9e49971ca355cbd87a806ac5aaf55ef97137fb9
Ross Rodwell <ross.rodwell@alces-software.com>
2020-02-03 16:22:29 +0000

    Upgraded check_vnc_running to support critical thresholds

--- nagios-plugins/check_vnc_running.sh
+++ nagios-plugins/check_vnc_running.sh
@@ -2,9 +2,10 @@
 
 
 warning_threshold_hours=$1
+critical_threshold_hours=$2
 
 #
-# Ensure the argument is an two digit integer - argument is in hours
+# Ensure the arguments are two digit integers - argument are in hours
 #
 
 valid_warning_threshold_hours=$(echo ${warning_threshold_hours} | grep -wP "[[:digit:]]{2}")
@@ -19,6 +20,24 @@ if [ ! ${valid_warning_threshold_hours} ]; then
     echo "Invalid Warning Threshold. Warning Threshold is: ${warning_threshold_hours}"
     exit 3
 fi
+
+valid_critical_threshold_hours=$(echo ${critical_threshold_hours} | grep -wP "[[:digit:]]{2}")
+rc=$?
+
+if [ ${rc} -ne 0 ]; then
+    echo "Error checking the warning threshold."
+    exit 3
+fi
+
+if [ ! ${valid_critical_threshold_hours} ]; then
+    echo "Invalid Warning Threshold. Warning Threshold is: ${critical_threshold_hours}"
+    exit 3
+fi
+
+if [ ${valid_critical_threshold_hours} -le ${valid_warning_threshold_hours} ]; then
+    echo "Critical Threshold should be greater than Warning Threshold!"
+    exit 3
+fi 
 
 #
 # See if vnc is running and grab its pid(s)
@@ -49,11 +68,15 @@ else
 
     seconds_per_hour="3600"
     warning_threshold_seconds=$((${valid_warning_threshold_hours} * ${seconds_per_hour}))
+    critical_threshold_seconds=$((${valid_critical_threshold_hours} * ${seconds_per_hour}))
 
     if [ ${elapsed_time} -lt ${warning_threshold_seconds} ]; then
         echo "OK: /usr/bin/Xvnc running for less than ${valid_warning_threshold_hours} hours."
         exit 0
-    elif [ ${elapsed_time} -ge ${warning_threshold_seconds} ]; then
+    elif [ ${elapsed_time} -ge ${critical_threshold_seconds} ]; then
+       echo "CRITICAL: /usr/bin/Xvnc running for more than ${valid_critical_threshold_hours} hours."
+       exit 2
+    elif [ ${elapsed_time} -ge ${warning_threshold_seconds} ] || [ ${elapsed_time} -lt ${critical_threshold_seconds} ]; then
        echo "WARNING: /usr/bin/Xvnc running for more than ${valid_warning_threshold_hours} hours."
        exit 1
     else
